831ec71d441e11a29b25941b546cdd61e9286845
kaleshrikant <kaleshrikant@live.com>
2023-12-18 17:20:53 +0530

    Adding validations

--- spring-boot-application-demo/src/main/java/com/example/spring/boot/controller/DepartmentController.java
+++ spring-boot-application-demo/src/main/java/com/example/spring/boot/controller/DepartmentController.java
@@ -2,6 +2,7 @@ package com.example.spring.boot.controller;
 
 import com.example.spring.boot.entity.Department;
 import com.example.spring.boot.service.DepartmentService;
+import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
@@ -15,7 +16,7 @@ public class DepartmentController {
     private DepartmentService departmentService;
 
     @PostMapping("/departments")
-    public Department saveDepartment(@RequestBody Department department){
+    public Department saveDepartment(@Valid @RequestBody Department department){
         return departmentService.saveDepartment(department);
     }
 
--- spring-boot-application-demo/src/main/java/com/example/spring/boot/entity/Department.java
+++ spring-boot-application-demo/src/main/java/com/example/spring/boot/entity/Department.java
@@ -4,10 +4,13 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.validation.constraints.*;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.hibernate.validator.constraints.Length;
+
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
@@ -17,6 +20,19 @@ public class Department {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     private Long departmentId;
+    @NotBlank (message = "DEPARTMENT NAME MUST NOT BE EMPTY")
+/*  @Length(max = , min = , message = "")
+    @Size
+    @Email
+    @Positive
+    @Negative
+    @PositiveOrZero
+    @NegativeOrZero
+    @Past
+    @PastOrPresent
+    @Future
+    @FutureOrPresent
+*/
     private String departmentName;
     private String departmentAddress;
     private String departmentCode;
