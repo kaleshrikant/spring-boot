3a75176ccaaefe7314cef8c668ed40ff3d548080
kaleshrikant <kaleshrikant@live.com>
2023-12-19 11:48:43 +0530

    spring-boot profiles

--- spring-boot-application-demo/src/main/resources/dbObjects.sql
+++ spring-boot-application-demo/src/main/resources/dbObjects.sql
@@ -16,3 +16,24 @@ mysql> show databases;
 
 mysql> use dcbapp;
 Database changed
+
+mysql> create database dcbapp_qa;
+Query OK, 1 row affected (0.01 sec)
+
+mysql> create database dcbapp_prod;
+Query OK, 1 row affected (0.01 sec)
+
+mysql> show databases;
++--------------------+
+| Database           |
++--------------------+
+| course_api         |
+| dcbapp             |
+| dcbapp_prod        |
+| dcbapp_qa          |
+| information_schema |
+| mysql              |
+| performance_schema |
+| sys                |
++--------------------+
+8 rows in set (0.00 sec)
