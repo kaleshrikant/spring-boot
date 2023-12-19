492c3e81a7139f5fa3e39e5cac55301ca7ec1f01
kaleshrikant <kaleshrikant@live.com>
2023-12-19 08:12:02 +0530

    Changing H2 DB to MySql

--- spring-boot-application-demo/src/main/java/com/example/spring/boot/entity/Department.java
+++ spring-boot-application-demo/src/main/java/com/example/spring/boot/entity/Department.java
@@ -12,8 +12,8 @@ import lombok.NoArgsConstructor;
 import org.hibernate.validator.constraints.Length;
 
 @Data
-@NoArgsConstructor
-@AllArgsConstructor
+//@NoArgsConstructor
+//@AllArgsConstructor
 @Entity
 @Builder
 public class Department {
@@ -36,4 +36,56 @@ public class Department {
     private String departmentName;
     private String departmentAddress;
     private String departmentCode;
+
+    public Department() {
+    }
+
+    public Department(Long departmentId, String departmentName, String departmentAddress, String departmentCode) {
+        this.departmentId = departmentId;
+        this.departmentName = departmentName;
+        this.departmentAddress = departmentAddress;
+        this.departmentCode = departmentCode;
+    }
+
+    public Long getDepartmentId() {
+        return departmentId;
+    }
+
+    public void setDepartmentId(Long departmentId) {
+        this.departmentId = departmentId;
+    }
+
+    public String getDepartmentName() {
+        return departmentName;
+    }
+
+    public void setDepartmentName(String departmentName) {
+        this.departmentName = departmentName;
+    }
+
+    public String getDepartmentAddress() {
+        return departmentAddress;
+    }
+
+    public void setDepartmentAddress(String departmentAddress) {
+        this.departmentAddress = departmentAddress;
+    }
+
+    public String getDepartmentCode() {
+        return departmentCode;
+    }
+
+    public void setDepartmentCode(String departmentCode) {
+        this.departmentCode = departmentCode;
+    }
+
+    @Override
+    public String toString() {
+        return "Department{" +
+                "departmentId=" + departmentId +
+                ", departmentName='" + departmentName + '\'' +
+                ", departmentAddress='" + departmentAddress + '\'' +
+                ", departmentCode='" + departmentCode + '\'' +
+                '}';
+    }
 }
--- spring-boot-application-demo/src/main/java/com/example/spring/boot/entity/ErrorMessage.java
+++ spring-boot-application-demo/src/main/java/com/example/spring/boot/entity/ErrorMessage.java
@@ -10,9 +10,41 @@ import org.springframework.http.HttpStatus;
 * */
 
 @Data
-@NoArgsConstructor
-@AllArgsConstructor
+//@NoArgsConstructor
+//@AllArgsConstructor
 public class ErrorMessage {
     private HttpStatus status;
     private String message;
+
+    public ErrorMessage() {
+    }
+
+    public ErrorMessage(HttpStatus status, String message) {
+        this.status = status;
+        this.message = message;
+    }
+
+    public HttpStatus getStatus() {
+        return status;
+    }
+
+    public void setStatus(HttpStatus status) {
+        this.status = status;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    @Override
+    public String toString() {
+        return "ErrorMessage{" +
+                "status=" + status +
+                ", message='" + message + '\'' +
+                '}';
+    }
 }
